cea78d3acaf79f65adae659fc070c8cdfb0fc0d7
EB387916 <EricButler@dswinc.com>
2014-10-15 14:31:54 -0400

    Fixed newline issue with the filename and errors line. Reformatted and renamed variables out of habit, no offense meant.

--- index.js
+++ index.js
@@ -1,37 +1,37 @@
-var fs = require('fs');
+var fs = require( 'fs' );
 
 var defaultFilename = 'jshint-output.log';
 
 var wrStream;
 var filename;
 
-module.exports = function (results, data, opts) {
-  opts = opts || {};
-  opts.filename = opts.filename || defaultFilename;
+module.exports = function( results, data, opts ) {
+	opts = opts || {};
+	opts.filename = opts.filename || defaultFilename;
 
-  if (wrStream && filename !== opts.filename) {
-    wrStream.end();
-    wrStream = null;
-  }
+	if( wrStream && filename !== opts.filename ) {
+		wrStream.end();
+		wrStream = null;
+	}
 
-  if (!wrStream) {
-    wrStream = fs.createWriteStream(opts.filename);
-    filename = opts.filename;
-  }
+	if( !wrStream ) {
+		wrStream = fs.createWriteStream( opts.filename );
+		filename = opts.filename;
+	}
+	outputNormalText( results, wrStream );
+};
 
-  var out = [];
-  var file = {};
-
-  results.forEach(function (result, i) {
-    var err = result.error;
-
-    if (i === 0) {
-      // start off the output with the filename
-      out.push(results.length + ' lint errors found in ' + result.file);
-    }
-
-    out.push('  [' + err.line + ',' + err.character + '](' + err.code + ') ' + err.reason);
-  });
-
-  wrStream.write(out.join('\n'));
+var outputNormalText = function outputNormalText( aoResults, wrStream ) {
+	var aOutput = [];
+	aoResults.forEach( function( oResult, i ) {
+		var oError = oResult.error;
+		if( i === 0 ) {
+			// Start each block with the number of errors and filename
+			aOutput.push( aoResults.length + ' lint errors found in ' + oResult.file );
+		}
+		// Indented entries ex. -> [186,2](W033) Missing semicolon.
+		aOutput.push( '\t[' + oError.line + ',' + oError.character + '](' + oError.code + ') ' + oError.reason );
+	} );
+	// Put it all together
+	wrStream.write( aOutput.join( '\n' ) + '\n' );
 };
